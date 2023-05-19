45e7bc8d9f4219c1a153b1487a0d8b79d3bb7f58
quintus <john@chaordo.com>
2023-05-19 14:53:11 -0600

    Initial Commit

---
+++ onboarding/onboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  onboardingApp.swift
+//  onboarding
+//
+//  Created by John on 5/19/23.
+//
+
+import SwiftUI
+
+@main
+struct onboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
